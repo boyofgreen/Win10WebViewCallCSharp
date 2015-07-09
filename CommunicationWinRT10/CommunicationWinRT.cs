f2948f7ee2c03f349f8b846dac593cc77ca11ed9
Jeff Burtoft <boyofgreen@gmail.com>
2015-07-09 16:04:34 -0500

    initial commit

---
+++ CommunicationWinRT10/CommunicationWinRT.cs
@@ -0,0 +1,121 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+
+namespace CommunicationWinRT
+{
+    [Windows.Foundation.Metadata.AllowForWeb]
+    public sealed class CommunicationWinRT
+    {
+        public CommunicationWinRT() 
+        {
+        }
+        public String GetValue()
+        {
+            return "From my WinRT component Yo!";
+        }
+    }
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Streams;
+using Windows.Foundation;
+
+namespace CommunicationWinRT
+{
+    [Windows.Foundation.Metadata.AllowForWeb]
+    public sealed class cameraWinRT
+    {
+        public cameraWinRT()
+        {
+        }
+
+        //public IAsyncOperation<string> CapturePicture()
+        //{
+        //    return getStringHelper().AsAsyncOperation();
+            
+        //}
+
+        //private async Task<string> getStringHelper()
+        //{
+        //    var dialog = new CameraCaptureUI();
+        //    var file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+        //    // Do something useful w/ the file
+        //    byte[] imageStream = await ToByteArrayAsync(file);
+        //    string base64String = Convert.ToBase64String(imageStream);
+        //    return base64String;
+            
+        //} 
+
+        
+        private static async Task<byte[]> ToByteArrayAsync(StorageFile file)
+        {
+            using (IRandomAccessStream stream = await file.OpenReadAsync())
+            {
+                using (DataReader reader = new DataReader(stream.GetInputStreamAt(0)))
+                {
+                    await reader.LoadAsync((uint)stream.Size);
+                    byte[] Bytes = new byte[stream.Size];
+                    reader.ReadBytes(Bytes);
+                    return Bytes;
+                }
+            }
+        }
+          
+
+    }
+
+
+    
+    
+    [Windows.Foundation.Metadata.AllowForWeb]
+    public sealed class CommunicationWinRT
+    {
+        public CommunicationWinRT()
+        {
+        }
+
+        public async void toastMessage(String message, int delay)
+        {
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode(message));
+
+            XmlNodeList toastImageAttributes = toastXml.GetElementsByTagName("image");
+            //((XmlElement)toastImageAttributes[0]).SetAttribute("src", "http://microsoft-chat.com/wp-content/uploads/2014/05/Microsoft_0-300x168.jpg");
+            //((XmlElement)toastImageAttributes[0]).SetAttribute("src", "https://raw.githubusercontent.com/seksenov/grouppost/master/images/AddNote.png");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("src", "https://raw.githubusercontent.com/seksenov/grouppost/master/images/logo.png");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("alt", "red graphic");
+
+            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "short");
+
+            XmlElement audio = toastXml.CreateElement("audio");
+            audio.SetAttribute("src", "ms-winsoundevent:Notification.IM");
+            toastNode.AppendChild(audio);
+
+            ((XmlElement)toastNode).SetAttribute("launch", "{\"type\":\"toast\",\"param1\":\"12345\",\"param2\":\"67890\"}");
+
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            await Task.Delay(delay);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+      
+    }
+}
---
+++ webviewApp/js/default.js
@@ -0,0 +1,34 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+	"use strict";
+
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
+
+	app.onactivated = function (args) {
+	    if (args.detail.kind === activation.ActivationKind.launch) {
+
+	        var webview = document.getElementById('webview');
+
+	        webview.style.height = '100%';
+	        webview.style.width = '100%';
+
+	        var communicationWinRT = new CommunicationWinRT.CommunicationWinRT();
+	        var cameraWinRT = new CommunicationWinRT.cameraWinRT();
+	        webview.addWebAllowedObject("jeffRocks", communicationWinRT);
+	       // webview.addWebAllowedObject("jeffRocks", cameraWinRT);
+	        webview.navigate("ms-appx-web:///page.html");
+	       // webview.navigate("https://testjeff.azurewebsites.net");
+
+		}
+	};
+
+	app.oncheckpoint = function (args) {
+		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+	};
+
+	app.start();
+})();
